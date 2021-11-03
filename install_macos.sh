3db4f31bea7190ce532292fbb97f084d54f50db3
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-11-03 10:44:37 +0100

    Add install script for macos

---
+++ install_macos.sh
@@ -0,0 +1,11 @@
+
+# Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install vim
+brew install vim
+brew install tmux
+
