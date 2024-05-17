d5e8b1468ad8eb996e9b82610e8d6b9b5f2a4113
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2024-05-17 22:23:33 +0200

    change git diff preview keybinding, was same as push

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -38,7 +38,7 @@ return {
       { "[g", "<CMD>Gitsigns prev_hunk<cr>", "Prev Hunk" },
       { "<leader>gs", "<CMD>Gitsigns stage_hunk<cr>",  desc = "Stage hunk"},
       { "<leader>gu", "<CMD>Gitsigns undo_stage_hunk<cr>",  desc = "Reset hunk"},
-      { "<leader>gp", "<CMD>Gitsigns preview_hunk<cr>",  desc = "Preview hunk"},
+      { "<leader>gd", "<CMD>Gitsigns preview_hunk<cr>",  desc = "Preview hunk"},
       { "<leader>gr", "<CMD>Gitsigns reset_hunk<cr>",  desc = "Reset hunk"},
       { "<leader>ga", "<CMD>Gitsigns stage_buffer<cr>",  desc = "Stage buffer"},
     },
