8ef4e5189de894dcc6eba374e39fa0c934dbb38a
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2024-11-07 14:14:32 +0100

    update theme definition

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,3 +1,4 @@
+-- cSpell:ignore tpope shiftwidth noremap nmap vmap imap autocmd augroup folke whichkey onedarkpro onedark olimorris reineke lukas blankline keybinds cpea nvim one_monokai Neovim
 return
 {
   -- NOTE: First, some plugins that don't require any configuration
@@ -8,24 +9,118 @@ return
 
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+	"<leader>?",
+	function()
+	  require("which-key").show({ global = false })
+	end,
+	desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
   { -- Theme inspired by Atom
     'cpea2506/one_monokai.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'one_monokai'
+      require('one_monokai').setup({
+        transparent = false,
+        vim.cmd("colorscheme one_monokai"),
+        colors = {
+          -- fg = "#ffffff",
+          fg = "#f6f7f8",
+          bg = "#282c34",
+          light_gray = "#9ca3b2",
+        },
+        themes = function(colors)
+          return {
+            Normal = {
+              fg = colors["fg"]:lighten(0.2),
+              bg = colors["bg"],
+            },
+          }
+        end
+      })
+      -- vim.builtin.lualine.options.theme = "onemonokai"
     end,
   },
-
-
+  -- { -- Onenord
+  --   'rmehri01/onenord.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     require('onenord').setup({
+  --         transparent = false,
+  --       })
+  --   end,
+  -- },
+  -- {
+  --   'rainglow/vim',
+  --   config = function()
+  --     vim.cmd("colorscheme absent-light")
+  --   end,
+  -- },
+  -- {
+  --   "f-person/auto-dark-mode.nvim",
+  --   requires = {
+  --     -- "rainglow/vim",
+  --     'cpea2506/one_monokai.nvim',
+  --     'rmehri01/onenord.nvim',
+  --   },
+  --   config = {
+  --     update_interval = 1000,
+  --     set_dark_mode = function()
+  --       vim.api.nvim_set_option("background", "dark")
+  --       vim.cmd("colorscheme one_monokai")
+  --       vim.builtin.lualine.options.theme = "onemonokai"
+  --     end,
+  --     -- set_light_mode = function()
+  --     --   vim.api.nvim_set_option("background", "light")
+  --     --   vim.cmd('colorscheme onenord')
+  --     --   vim.builtin.lualine.options.theme = "onenord"
+  --     -- end,
+  --     -- set_light_mode = function()
+  --     --   vim.api.nvim_set_option("background", "dark")
+  --     --   vim.cmd("colorscheme one_monokai")
+  --     --   vim.builtin.lualine.options.theme = "onemonokai"
+  --     -- end,
+  --     set_light_mode = function()
+  --       vim.api.nvim_set_option("background", "light")
+  --       vim.cmd('colorscheme absent-light')
+  --       vim.builtin.lualine.options.theme = "onemonokai"
+  --     end,
+  --   },
+  -- },
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = {
+        char = "▏",
+        -- smart_indent_cap = true,
+        -- highlight = "IndentBlanklineChar",
+      },
+      scope = {
+        enabled = true,
+        char = "▎",
+        -- highlight = "IndentBlanklineScope",
+        show_start = true,
+        show_end = true,
+      },
     },
+    config = function(_, opts)
+      require("ibl").setup(opts)
+      -- vim.cmd.highlight('clear @ibl.scope.underline.1')
+      -- vim.cmd.highlight('link @ibl.scope.underline.1 MyHighlightGroup')
+    end,
   },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
@@ -42,5 +137,9 @@ return
   --
   --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
   --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
-  { import = 'custom.plugins' },
-}, {}
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+  -- { import = 'custom.plugins' },
+}
